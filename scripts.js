a206ea4eaec7786e0f614d775ffa12a967235032
Kaif Khan <92052609+Kephh@users.noreply.github.com>
2024-01-20 23:53:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-const resumeFilePath = './assests/resume.pdf';
-    
-        const nameText = document.getElementById('name-text');
-    
-        nameText.addEventListener('click', function() {
-          const link = document.createElement('a');
-          link.href = resumeFilePath;
-          link.download = 'Kaif_Resume.pdf';
-          document.body.appendChild(link);
-    
-          link.click();
-    
-          document.body.removeChild(link);
-        });
+const resumeFilePath = './assets/resume.pdf';
+const nameText = document.getElementById('name-text');
+const crsr = document.querySelector(".cursor");
+
+let pos=0;
+
+nameText.addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = resumeFilePath;
+  link.download = 'Kaif_Resume.pdf';
+  document.body.appendChild(link);
+
+  link.click();
+
+  document.body.removeChild(link);
+});
+
+document.addEventListener("mousemove", function (e) {
+  crsr.style.left = e.clientX + "px";
+  crsr.style.top = e.clientY + "px";
+  pos=e.clientY;
+});
+
+document.addEventListener("scroll", function () {
+  scrolledPixelsY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+  crsr.style.top = pos + scrolledPixelsY + "px";
+});
